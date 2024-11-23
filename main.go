5f30e1ff021b46a6512d25d2c9ca34f70bcabaa8
José Antonio Garcel <garcel.developer@gmail.com>
2024-11-23 10:25:11 +0100

    feat: Include lines and bytes flags

--- main.go
+++ main.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 )
 
+type Mode int
+
+const (
+	Words Mode = iota
+	Lines
+	Bytes
+)
+
 func main() {
-	fmt.Println(count(os.Stdin))
+	mode := parseFlags()
+
+	fmt.Println(count(os.Stdin, mode))
 }
 
-func count(r io.Reader) int {
+func parseFlags() Mode {
+	lines := flag.Bool("l", false, " Count lines")
+	bytes := flag.Bool("b", false, " Count bytes")
+	flag.Parse()
+
+	if *lines && *bytes {
+		log.Fatal("both lines and bytes modes enabled at the same time. Please enable just one at once")
+	}
+
+	mode := Words
+	if *lines {
+		mode = Lines
+
+	} else if *bytes {
+		mode = Bytes
+	}
+
+	return mode
+}
+
+func count(r io.Reader, mode Mode) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+	if mode == Words {
+		scanner.Split(bufio.ScanWords)
+
+	} else if mode == Bytes {
+		scanner.Split(bufio.ScanBytes)
+	}
+
 	wc := 0
 
 	for scanner.Scan() {
--- main_test.go
+++ main_test.go
@@ -8,7 +8,31 @@ import (
 func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	exp := 4
-	res := count(b)
+
+	res := count(b, Words)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\n line2\nline3 word1")
+	exp := 3
+
+	res := count(b, Lines)
+
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\n line2\nline3 word1")
+	exp := 36
+
+	res := count(b, Bytes)
+
 	if res != exp {
 		t.Errorf("Expected %d, got %d instead.\n", exp, res)
 	}
